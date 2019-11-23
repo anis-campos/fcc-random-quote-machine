84b0cb120dd90d41ef6cfc21ba228f18a19b68db
Anis DA SILVA CAMPOS <yocorporation@hotmail.com>
2019-11-22 19:07:42 -0500

    Building solution

---
+++ build/precache-manifest.70cf1327aaa6cc0db7623469ee1901ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f976ec12c7b5c680f1a2e2c6b32590e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b246751f9a9d2b68b5c",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "616bec24dba9d0b10fe4",
+    "url": "/static/js/2.df4452af.chunk.js"
+  },
+  {
+    "revision": "3b246751f9a9d2b68b5c",
+    "url": "/static/js/main.031fc795.chunk.js"
+  },
+  {
+    "revision": "205515ed6b470a161633",
+    "url": "/static/js/runtime-main.08af614c.js"
+  }
+]);
