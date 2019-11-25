e88bd0567936dcfe29c915f7eb49ddca805b3304
Anis DA SILVA CAMPOS <yocorporation@hotmail.com>
2019-11-25 11:39:28 -0500

    Updates

---
+++ precache-manifest.23317c6f36d54e52e139a84ae01d5ee4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba9e30a02094c5b041aa8511000f4f14",
+    "url": "/fcc-random-quote-machine/index.html"
+  },
+  {
+    "revision": "2f16b4a02a6cd11c6ce8",
+    "url": "/fcc-random-quote-machine/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a0a718fdc879ce21ee8a",
+    "url": "/fcc-random-quote-machine/static/js/2.b45143b5.chunk.js"
+  },
+  {
+    "revision": "2f16b4a02a6cd11c6ce8",
+    "url": "/fcc-random-quote-machine/static/js/main.95839645.chunk.js"
+  },
+  {
+    "revision": "4ecd7dd7dc9e185574e8",
+    "url": "/fcc-random-quote-machine/static/js/runtime-main.7e1e0525.js"
+  }
+]);
